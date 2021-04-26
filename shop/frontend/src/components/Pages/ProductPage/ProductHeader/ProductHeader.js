6e319f2ac494656a8d8bdfe7027f1747191009e1
Omar Ali <omar199590@gmail.com>
2021-04-26 21:38:12 +0200

    deploy13

--- shop/frontend/src/components/Pages/ProductPage/ProductHeader/ProductHeader.js
+++ shop/frontend/src/components/Pages/ProductPage/ProductHeader/ProductHeader.js
@@ -4,50 +4,8 @@ import { connect } from 'react-redux';
 import { addtoLocalCart, removeFromLocalCart } from '../../../../actions/products';
 
 class ProductHeader extends Component {
-    state = {
-        inCart: false
-    }
-
-    componentDidMount() {
-        let item;
-        for(item in this.props.localCart) {
-            if(this.props.productinfo.id === item.id) {
-                this.setState({
-                    inCart: true
-                })
-            }
-        }
-    }
-
-    componentDidUpdate(prevProps) {
-        if(this.props.localCart !== prevProps.localCart) {
-            let item;
-            for(item of this.props.localCart) {
-                if(this.props.productinfo.id === item.id) {
-                    this.setState({
-                        inCart: true
-                    })
-                    return;
-                }
-            }
-            this.setState({
-                inCart: false
-            }) 
-        }
-    }
-
     addtocart = () => {
-        if(this.state.inCart) {
-            this.props.removeFromLocalCart(this.props.productinfo.id);
-            this.setState({
-                inCart: false
-            })
-        } else {
-            this.props.addtoLocalCart(this.props.productinfo);
-            this.setState({
-                inCart: true
-            })
-        }
+        this.props.addtoLocalCart(this.props.productinfo);
     }
 
     render() {
@@ -58,7 +16,7 @@ class ProductHeader extends Component {
                         <h3>{this.props.productinfo.price}$</h3>
                         <h1>{this.props.productinfo.name}</h1>
                     </div>
-                    <button onClick={this.addtocart} className={classes.cartbutton}>{this.state.inCart ? 'REMOVE FROM CART' : 'ADD TO CART'}</button>
+                    <button onClick={this.addtocart} className={classes.cartbutton}>ADD TO CART</button>
                 </div>
                 <div className={classes.img}>
                     <img src={this.props.productinfo.picture1}/>
