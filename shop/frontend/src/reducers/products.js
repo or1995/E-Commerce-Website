6d1d7895c32989f47df5504e7211fb7d05a66086
Omar Ali <omar199590@gmail.com>
2021-04-26 21:23:00 +0200

    deploy12

--- shop/frontend/src/reducers/products.js
+++ shop/frontend/src/reducers/products.js
@@ -98,6 +98,7 @@ export default function(state = initialState, action) {
             }
         case ADD_ITEM_TO_LOCAL_CART:
             // the problem is here. has to do with the cart not being defind in localstorage
+            console.log(JSON.parse(localStorage.getItem("cart")));
             let newArr = [...JSON.parse(localStorage.getItem("cart")), action.cartItem];
             localStorage.setItem("cart", JSON.stringify(newArr));
             return {
